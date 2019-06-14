972d4bb370ade5d942dfa7b8e1d4c3d1ea98387a
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2019-06-14 08:28:16 +0800

    Update MCDC.py

--- MCDC.py
+++ MCDC.py
@@ -1,10 +1,10 @@
 # coding=utf-8
 import xlrd
 import xlwt
-read_book = xlrd.open_workbook(r'C:\Users\he_junke\Desktop\HSI\BP512\C919-TC-HSI-S011(LOC Course angle).xlsx')
-write_book = xlwt.Workbook(r'C:\Users\he_junke\Desktop\HSI\BP512\C919-TC-HSI-S011(LOC Course angle).xlsx')
+read_book = xlrd.open_workbook(r'需要的.xlsx')
+write_book = xlwt.Workbook(r'需要的.xlsx')
 sheet = read_book.sheet_by_index(3)
-write_sheet = write_book.add_sheet("IV Test Cases1")
+write_sheet = write_book.add_sheet("需要的")
 orgin_number = 0
 list_set = []
 list_value = []
@@ -25,7 +25,7 @@ for i in range(0, orgin_number):
     write_sheet.write(i, 2, list_set[i])
     write_sheet.write(i, 3, list_value[i])
 write_sheet.write(orgin_number, 1, "VERIFY")
-write_sheet.write(orgin_number, 2, "HSI shall display TFC")
+write_sheet.write(orgin_number, 2, "显示")
 # 第一行显示
 for mid in range(0, orgin_number):
     for i in range((mid + 1) * orgin_number + 1 + mid, mid + (mid + 1) * orgin_number + 1 + mid):
@@ -42,7 +42,7 @@ for mid in range(0, orgin_number):
         write_sheet.write(i, 2, list_set[i - ((mid + 1) * orgin_number + 1 + mid)])
         write_sheet.write(i, 3, list_value[i - ((mid + 1) * orgin_number + 1 + mid)])
     write_sheet.write(orgin_number + (mid + 1) * orgin_number + 1 + mid, 1, "VERIFY")
-    write_sheet.write(orgin_number + (mid + 1) * orgin_number + 1 + mid, 2, "HSI shall remove TFC")
+    write_sheet.write(orgin_number + (mid + 1) * orgin_number + 1 + mid, 2, "不显示")
 # 中间阶段不显示
 for i in range(orgin_number + (orgin_number + 1) * orgin_number + 1,
                orgin_number + orgin_number + (orgin_number + 1) * orgin_number + 1):
@@ -52,7 +52,7 @@ for i in range(orgin_number + (orgin_number + 1) * orgin_number + 1,
 write_sheet.write(orgin_number + orgin_number + (orgin_number + 1) * orgin_number + 1, 1, "VERIFY")
 write_sheet.write(orgin_number + orgin_number + (orgin_number + 1) * orgin_number + 1, 2, "clear")
 # 最后阶段消失
-write_book.save(r'python_HSI_MCDC1.xlsx')
+write_book.save(r'需要的1.xlsx')
 
 
 
