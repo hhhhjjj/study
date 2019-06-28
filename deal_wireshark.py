f560756afcf342db09bdb07d2b2aadbd18ca8356
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2019-06-28 17:35:28 +0800

    firstly, you need turn ntar of wireshark to json

--- deal_wireshark.py
+++ deal_wireshark.py
@@ -1,6 +1,23 @@
-import re
 import json
-with open('data.json', 'rb') as load_json:
-    # this is use rb not r,otherwise could UnicodeDecodeError
-    load_all = json.load(load_json)
-    print(load_all)
+import os
+if os.path.exists("data.txt"):
+    os.remove("data.txt")
+#  if have file, first delete
+
+
+def wireshark_to_packet():
+    f = open("data.txt", "a")
+    with open('data.json', 'rb') as load_json:
+        # this is use rb not r,otherwise could UnicodeDecodeError
+        load_all = json.load(load_json)
+        # load_all is a list
+        for package in load_all[:-1]:
+            # last column alone write to avoid /n in txt
+            data_16 = package['a']['b']['c']['d'][23:-12].replace(":", ",0x")[1:]
+            # find data and turn to 16
+            f.write("[" + data_16 + "]" + "\n")
+        f.write("[" + load_all[-1]['a']['b']['c']['d'][23:-12].replace(":", ",0x")[1:] + "]")
+    f.close()
+
+
+wireshark_to_packet()
