9c9553c55b7dc26870489fa48e8c4bf36f5d3e75
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2019-08-26 16:34:30 +0800

    Add files via upload

--- payload.py
+++ payload.py
@@ -1,14 +1,14 @@
 import os
 # wireshark use particular group export to txt
 
-if os.path.exists("hhhhh_data.txt"):
-    os.remove("hhhhh_data.txt")
+if os.path.exists(r"D:\wireshark_data.txt"):
+    os.remove(r"D:\wireshark_data.txt")
 
 
 def wireshark_to_packet():
-    write_F = open("hhhhh_data.txt", "a")
+    write_F = open(r"D:\wireshark_data.txt", "a")
     i = 0
-    with open("hhhhh.txt") as f:
+    with open(r"D:\wireshark_init.txt") as f:
         for every_package in f.readlines():
             if i % 4 == 2:
                 # remainder equal to 2
@@ -16,10 +16,11 @@ def wireshark_to_packet():
                 write_F.write("[" + data_16 + "],")
             i = i + 1
     write_F.close()
-    # delete the last ,
-    with open("hhhhh_data.txt", 'rb+') as filehandle:
-        filehandle.seek(-1, os.SEEK_END)
-        filehandle.truncate()
+    # # delete the last ,
+    # 这个不删除也可以,因为打包成exe之后这个命令会报错的
+    # with open(r"D:\wireshark_data.txt", 'rb+') as filehandle:
+    #     filehandle.seek(-1, os.SEEK_END)
+    #     filehandle.truncate()
 
 
 wireshark_to_packet()
